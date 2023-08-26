3cc7bf97196ca3ff4879a899945c71754da6ceb5
Unagi11 <unagi11@naver.com>
2023-08-26 16:10:03 +0900

    dd

--- src/dialog.ts
+++ src/dialog.ts
@@ -6,6 +6,9 @@ import { Helper } from './helper';
 export class Dialog extends ObjectBase {
 
     private static _instance: Dialog;
+    text_update ?: (delta: number) => void;
+    get texting () { return !!this.text_update; }
+
     static get instance(): Dialog {
         if (!Dialog._instance || !Dialog._instance.root.parent) {
             Dialog._instance = new Dialog(global.root);
@@ -47,29 +50,35 @@ export class Dialog extends ObjectBase {
         Dialog.play(texts[index]);
         
         Helper.addTouchEndEvent(Dialog.instance.panel, _ => {
-            index++;
-            if (index >= texts.length) Dialog.instance.root.visible = false;
-            else Dialog.play(texts[index]);
+            if (Dialog.instance.texting) Dialog.end(texts[index]);
+            else if (index < texts.length - 1) Dialog.play(texts[++index]);
+            else Dialog.instance.root.visible = false;
         })
     }
 
     static play(text : string) {
-        
         let global_app = global.app as PIXI.Application;
-
         let time = 0;
+
         let text_update = (delta : number) => {
             time += delta / 2;
             let len = Math.floor(time);
             Dialog.instance.text_box.text = text.slice(0, len);
 
-            if (len >= text.length) {
-                Dialog.instance.text_box.text = text;
-                global_app.ticker.remove(text_update);
-            }
+            if (len >= text.length) this.end(text);
         }
 
         global_app.ticker.add(text_update);
+        Dialog.instance.text_update = text_update;
         console.log(text)
+    }
+
+    static end(text : string) {
+        if (!Dialog.instance.text_update) return;
+        let global_app = global.app as PIXI.Application;
+
+        Dialog.instance.text_box.text = text;
+        global_app.ticker.remove(Dialog.instance.text_update);
+        Dialog.instance.text_update = undefined;
     }
 }
--- src/helper.ts
+++ src/helper.ts
@@ -1,8 +1,13 @@
 import { Container, FederatedEventHandler } from 'pixi.js';
 
 export class Helper {
-    static addTouchEndEvent(btn : Container, func : FederatedEventHandler) {
+
+    static addTouchEndEvent(btn : Container, func : FederatedEventHandler, cursor : boolean = false) {
         btn.interactive = true;
         btn.onpointerdown = func;
+        btn.onpointerover = () => { btn.cursor = 'pointer'; };
+        btn.onpointerleave = () => {
+            console.log('leave');
+            btn.cursor = 'default'; };
     }
 }
